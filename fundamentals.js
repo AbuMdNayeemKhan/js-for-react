fcba6ac9bf538243a5922127bafc7d59f9e64dfe
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-03-06 22:37:21 +0600

    added

---
+++ fundamentals.js
@@ -0,0 +1,34 @@
+// 1. VARIABLE
+// how to declare a variable using let and const
+const fathersName = "A. Mannan Khan"; // this types of variables is not will be change.
+let season = 'rainy'; // this types of variables is chaingable.
+
+// 2. CONDITION
+// there are 6 types of condition >, <, ===, !==, <=, >=
+// multiple conditions: &&, ||
+if(true)
+{
+
+} else if(true){
+
+}else{};
+
+// 3. ARRAY
+// index, length, push
+const numbers = [89, 23, 98, 38];
+numbers[0] = 56;
+
+// 4. LOOP
+
+// 5. FUNCTION
+
+// 6. OBJECT
+
+// ES6 =>
+    // 1. TEMPLATE STRING
+    // 2. ARROW FUNCTION
+        // 1.MAP
+        // 2.FOR EACH 
+        // 3.FILTER 
+        // 4.FIND  
+    // 3. SPREAD OPERATOR
